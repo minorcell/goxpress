51186d7b1fd1c50c3d25a6dd00765e3b81c150cb
minorcell <minorcell6789@gmail.com>
2025-08-02 12:36:04 +0800

    docs: add annotation content

--- examples/hello_world/main.go
+++ examples/hello_world/main.go
@@ -1,14 +1,18 @@
 package main
 
+// import goxpress
 import "github.com/minorcell/goxpress"
 
 func main() {
+	// Step 1: Create a new instance of goxpress.Engine
 	app := goxpress.New()
 
+	// Step 2: Define routes, goxexpress supports all HTTP methods
 	app.GET("/", func(c *goxpress.Context) {
 		c.String(200, "Hello, World!")
 	})
 
+	// Step 3: Start the server, you can specify the running port and the fallback
 	app.Listen(":8080", func() {
 		println("Server running at http://localhost:8080")
 	})
